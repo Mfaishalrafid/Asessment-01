d4870f300cb79bf68e133516e9e2bd9eeb72e1e9
M.faishal rafid <rafid99x@gmail.com>
2024-02-29 14:56:31 +0700

    menambahkan top up bar

--- app/src/main/java/org/d3if3073/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3073/mobpro1/MainActivity.kt
@@ -1,16 +1,25 @@
 package org.d3if3073.mobpro1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3073.mobpro1.ui.theme.Mobpro1Theme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,17 +39,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    MainScreen {modifier ->
+        Text(
+            text = "$name",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     Mobpro1Theme {
         Greeting("M.Faishal Rafid\n6706223073\n46-03")
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(content: @Composable (Modifier) -> Unit) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name ))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { padding ->
+        content(Modifier.padding(padding))
+    }
+}
+
+
