f5c191c242d24772892ba54bdb023d1f617a24df
M.faishal rafid <rafid99x@gmail.com>
2024-02-29 13:57:23 +0700

    first commit

---
+++ app/src/main/java/org/d3if3073/mobpro1/MainActivity.kt
@@ -0,0 +1,46 @@
+package org.d3if3073.mobpro1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3073.mobpro1.ui.theme.Mobpro1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Mobpro1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("M.Faishal Rafid\n6706223073\n46-03")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Mobpro1Theme {
+        Greeting("M.Faishal Rafid\n6706223073\n46-03")
+    }
+}
