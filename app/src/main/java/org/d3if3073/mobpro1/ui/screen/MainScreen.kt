2b12475de297d89cd6c6b7432dc038167de4e298
M.faishal rafid <rafid99x@gmail.com>
2024-03-14 11:24:13 +0700

    Memisahkan Composable dari Activity

---
+++ app/src/main/java/org/d3if3073/mobpro1/ui/screen/MainScreen.kt
@@ -0,0 +1,94 @@
+package org.d3if3073.mobpro1.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.unit.dp
+import org.d3if3073.mobpro1.R
+
+
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3073.mobpro1.ui.theme.Mobpro1Theme
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Mobpro1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun MainScreen(name: String) {
+//    MainScreen {modifier ->
+//        Text(
+//            text = "$name",
+//            modifier = modifier
+//        )
+//    }
+//}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ScreenPreview() {
+    Mobpro1Theme {
+        MainScreen()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name ))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { padding ->
+        ScreenContent(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier) {
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ) {
+
+    }
+}
+
