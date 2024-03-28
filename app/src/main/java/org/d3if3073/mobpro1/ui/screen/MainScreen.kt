4d0157307eaa953ce87afda35654a0f8a9ecb7d9
M.faishal rafid <rafid99x@gmail.com>
2024-03-28 13:15:59 +0700

    Membuat tampilan list

--- app/src/main/java/org/d3if3073/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3073/mobpro1/ui/screen/MainScreen.kt
@@ -1,8 +1,6 @@
 package org.d3if3073.mobpro1.ui.screen
 
-import android.app.LauncherActivity.ListItem
 import androidx.compose.foundation.layout.Column
-import androidx.compose.ui.unit.dp
 import org.d3if3073.mobpro1.R
 
 
@@ -11,9 +9,13 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -21,14 +23,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if3073.mobpro1.model.Catatan
+import org.d3if3073.mobpro1.screen.MainViewModel
 import org.d3if3073.mobpro1.ui.theme.Mobpro1Theme
-import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,20 +93,41 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    Column (
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+        LazyColumn (
+            modifier = modifier
+                .fillMaxSize()
 
+
+        ) {
+            items (data) {
+                ListItem(catatan = it)
+                Divider()
+            }
+        }
     }
-}
+
+
+
 
 @Composable
 fun ListItem(catatan: Catatan) {
-    Column {
-        Text(text = catatan.judul)
-        Text(text = catatan.catatan)
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Text(
+            text = catatan.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = catatan.catatan,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
         Text(text = catatan.tanggal)
     }
 }
